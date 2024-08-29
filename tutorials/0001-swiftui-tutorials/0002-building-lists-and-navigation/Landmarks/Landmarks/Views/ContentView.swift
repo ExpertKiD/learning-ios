fa1a5d226689c5f8dbe1c9f2b05755d2546b84f6
Suman Adhikari <expertxkid@gmail.com>
2024-08-29 07:15:16 -0500

    feat/tutorials: complete 0002-building-lists-and-navigation

---
+++ tutorials/0001-swiftui-tutorials/0002-building-lists-and-navigation/Landmarks/Landmarks/Views/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Suman Adhikari on 8/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+       LandmarkList()
+    }
+}
+
+#Preview {
+    ContentView()
+}
